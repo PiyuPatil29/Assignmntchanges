202b74605398c2b2f5f475fe0cf073b7818e77e4
PiyuPatil29 <piyunish91@gmail.com>
2021-01-11 21:31:33 +0530

    Chang Updated commit

---
+++ MavenDemo/src/main/java/assignment1/Assignment1_4.java
@@ -0,0 +1,28 @@
+package assignment1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment1_4 {
+	static WebDriver driver;
+
+	@Test
+	public void m1() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_button_disabled");
+		driver.manage().window().maximize();
+		driver.switchTo().frame("iframeResult");
+		WebElement button = driver.findElement(By.xpath("/html/body/button"));
+		Assert.assertEquals(false, button.isEnabled());// button is disabled
+		System.out.println("Button click me is disabled "+button.getText());
+
+	}
+
+}
